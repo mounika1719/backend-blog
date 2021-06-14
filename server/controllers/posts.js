cfea1f13e593de3be89216b8a41e2f98026b6178
mounika1719 <71956696+mounika1719@users.noreply.github.com>
2021-06-14 14:53:46 +0530

    Update posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -10,22 +10,20 @@ export const getPosts = async (req, res) => {
         const postMessages = await PostMessage.find();
                 
         res.status(200).json(postMessages);
-    } catch (error) {
+        }   catch (error) {
         res.status(404).json({ message: error.message });
-    }
+        }
 }
 
 export const getPost = async (req, res) => { 
     const { id } = req.params;
-
     try {
-        const post = await PostMessage.findById(id);
-        
+        const post = await PostMessage.findById(id);  
         res.status(200).json(post);
-    } catch (error) {
+        } catch (error) {
         res.status(404).json({ message: error.message });
-    }
-}
+        }
+} 
 
 export const createPost = async (req, res) => {
     const post = req.body;
