661a3aa6faa1854072e2c94b244ce73dca22389f
mounika1719 <71956696+mounika1719@users.noreply.github.com>
2021-06-28 16:26:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,29 +3,21 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
-
 const app = express();
 dotenv.config();
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
-
-
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
-
 app.get('/', (req,res)=>{
   res.send('hello to my api')
 })
 //const CONNECTION_URL = 'mongodb+srv://mynotes:mouni1998@cluster0.ya5w9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 8000;
-
 mongoose.connect( process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
-
   mongoose.set('useFindAndModify', false);
