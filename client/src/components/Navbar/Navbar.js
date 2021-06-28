f89140a2dce73c0f1223651e475ddffbc1db53d0
mounika1719 <71956696+mounika1719@users.noreply.github.com>
2021-06-28 16:18:16 +0530

    Update Navbar.js

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -6,7 +6,6 @@ import decode from 'jwt-decode';
 import mypic from '../../images/mypic.png';
 import * as actionType from '../../constants/actionTypes';
 import useStyles from './styles';
-
 const Navbar = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
   const dispatch = useDispatch();
@@ -16,24 +15,18 @@ const Navbar = () => {
 
   const logout = () => {
     dispatch({ type: actionType.LOGOUT });
-
     history.push('/auth');
-
     setUser(null);
   };
 
   useEffect(() => {
     const token = user?.token;
-
     if (token) {
       const decodedToken = decode(token);
-
       if (decodedToken.exp * 1000 < new Date().getTime()) logout();
     }
-
     setUser(JSON.parse(localStorage.getItem('profile')));
   }, [location]);
-
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
