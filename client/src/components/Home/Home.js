660c09a2fc594609dc74fd12f0c5cf5436740702
mounika1719 <71956696+mounika1719@users.noreply.github.com>
2021-06-28 16:17:37 +0530

    Update Home.js

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
-
 import { getPosts } from '../../actions/posts';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
@@ -9,11 +8,9 @@ import Form from '../Form/Form';
 const Home = () => {
   const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
-
   useEffect(() => {
     dispatch(getPosts());
   }, [currentId, dispatch]);
-
   return (
     <Grow in>
       <Container>
@@ -29,5 +26,4 @@ const Home = () => {
     </Grow>
   );
 };
-
 export default Home;
